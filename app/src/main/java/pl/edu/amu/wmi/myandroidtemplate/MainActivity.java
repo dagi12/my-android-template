c9e2fa2f6a61811affb553c323c6f7bf9a71624a
dagi12 <dagi12@o2.pl>
2017-10-21 14:10:56 +0200

    initial commit

---
+++ app/src/main/java/pl/edu/amu/wmi/myandroidtemplate/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.wmi.myandroidtemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
