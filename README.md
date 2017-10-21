406b8c8ebe344e8ed83794b87d9ecab32d20fa3b
dagi12 <dagi12@o2.pl>
2017-10-21 15:48:26 +0200

    crashlytics, diawi, sonar

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # my-android-template
 
 I often do same thing on new android project. Here is a tempalte for that.
+
+## Demo debug
+
+$ ./gradlew clean assembleDebug diawiPublishDebug
+
+## Building for Play Store
+
+$ ./gradlew clean assembleRelease diawiPublishRelease
---
+++ app/src/main/java/pl/edu/amu/wmi/myandroidtemplate/app/MyApplication.java
@@ -0,0 +1,15 @@
+package pl.edu.amu.wmi.myandroidtemplate.app;
+
+import pl.edu.amu.wmi.erykandroidcommon.di.CommonApplication;
+
+/**
+ * @author Eryk Mariankowski <eryk.mariankowski@247.codes> on 21.10.17.
+ */
+public class MyApplication extends CommonApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
